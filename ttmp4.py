41534d0c0b115e73c3712d75b4c16aeabd5dd4c6
alyssadev <alyssa.dev.smith@gmail.com>
2023-04-28 00:07:41 +1000

    fixed dodgy url support

--- ttmp4.py
+++ ttmp4.py
@@ -5,23 +5,26 @@ from bs4 import BeautifulSoup as Soup
 from requests import get as _get
 from re import match
 from os.path import isfile
+from secrets import token_urlsafe
 
 app = Flask(__name__)
+app.secret_key = token_urlsafe(16)
 
 @app.route("/")
 def index():
-    url = request.args.get("url").split("?")[0]
+    url = request.args.get("url", "").split("?")[0]
     if not url:
         listing = Popen(["bash", "-c", "ls -tc static/*.webp"], stdout=PIPE)
         out, err = listing.communicate()
         out = out.decode("utf-8").strip()
         return render_template("index.html", listing=[l[:-5] for l in out.split()][:100])
-    if url.startswith("https://vm.tiktok.com/") or url.startswith("https://vt.tiktok.com/"):
-        curl = Popen(["curl", url], stdout=PIPE, stderr=PIPE)
-        out, err = curl.communicate()
-        url = Soup(out).find("a")["href"].split("?")[0]
+    if not match("^https?://([a-z0-9]+[.])*tiktok.com", url):
+        flash(f"not a tiktok url: {url}")
+        return redirect(url_for("index"))
+    url = _get(url).url.split("?",1)[0]
     if not match("https://www.tiktok.com/@[a-zA-Z0-9\._]+?/video/\d+", url):
-        return "bad url"
+        flash(f"bad url: {url}")
+        return redirect(url_for("index"))
     if url[-1] == "/":
         url = url[:-1]
     if not isfile("static/" + url.split("/")[-1] + ".mp4"):
