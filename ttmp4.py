f831a11cc3c5565de27dee144908506f2256983a
Alyssa Smith <63546997+alyssadev@users.noreply.github.com>
2023-06-03 18:46:16 +1000

    Implement CDN, thumbnail param

--- ttmp4.py
+++ ttmp4.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
+from urllib.parse import urljoin
 from flask import *
 from subprocess import Popen, PIPE
 from bs4 import BeautifulSoup as Soup
 from requests import get as _get
 from re import match
+from os import environ
 from os.path import isfile
 from secrets import token_urlsafe
 
 app = Flask(__name__)
 app.secret_key = token_urlsafe(16)
+
+@app.endpoint('static')
+def static(filename):
+    return redirect(urljoin("https://cdn.aly.pet/tt/", filename))
+#   return app.send_static_file(filename)
 
 @app.route("/")
 def index():
@@ -17,13 +24,13 @@ def index():
         listing = Popen(["bash", "-c", "ls -tc static/*.webp"], stdout=PIPE)
         out, err = listing.communicate()
         out = out.decode("utf-8").strip()
-        return render_template("index.html", listing=[l[:-5] for l in out.split()][:100])
+        return render_template("index.html", listing=[l[:-5] for l in out.split()][:100], thumbs="thumbs" in request.args)
     if not match("^https?://([a-z0-9]+[.])*tiktok.com", url):
-        flash(f"not a tiktok url: {url}")
+        flash(f"not a tiktok.com url")
         return redirect(url_for("index"))
     url = _get(url).url.split("?",1)[0]
     if not match("https://www.tiktok.com/@[a-zA-Z0-9\._]+?/video/\d+", url):
-        flash(f"bad url: {url}")
+        flash(f"bad url acquired after resolving redirects: {url}")
         return redirect(url_for("index"))
     if url[-1] == "/":
         url = url[:-1]
@@ -33,8 +40,11 @@ def index():
         out, err = ytdl.communicate()
         code = ytdl.wait()
         if code != 0:
-            return "not a video url"
-    return redirect(url_for("static", filename=url.split("/")[-1] + ".mp4"))
+            flash("not a video url")
+            return redirect(url_for("index"))
+        rsync = Popen(["rsync", "-vaP", "static/", environ["CDN_PATH"]])
+        code = ytdl.wait()
+    return redirect(url_for("static", filename=url.split('/')[-1] + ".mp4"))
 
 if __name__ == "__main__":
     app.run(port=51761, host="0.0.0.0")
